4675a4487b0f396afe450ddeed9866e9a5c8b191
matteobonelli <matteobonelli08@gmail.com>
2023-10-23 15:42:56 +0200

    now the whole team cards are visualized in DOM

--- js/script.js
+++ js/script.js
@@ -1,3 +1,16 @@
+/*
+            <div class="col-4 d-flex justify-content-center ">
+                <div class="card">
+                    <img src="img/angela-caroll-chief-editor.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                      <h5 class="card-title">Angela Carrol</h5>
+                      <p class="card-text">Me faccio le pippe a coccia d'ovo</p>
+                    </div>
+                </div>
+            </div>
+*/
+
+const rowEl = document.querySelector('.row');
 let ourTeam = [
     {
         name : 'Wayne Barnett',
@@ -32,10 +45,31 @@ let ourTeam = [
 ];
 
 for (let i = 0; i < ourTeam.length; i++){
-    console.group();
-    console.log(ourTeam[i].name);
-    console.log(ourTeam[i].role);
-    console.log(ourTeam[i].picture);
-    console.groupEnd();
+    const memberCard = generateCard(ourTeam[i])
+    rowEl.append(memberCard);
+    // console.group();
+    // console.log(ourTeam[i].name);
+    // console.log(ourTeam[i].role);
+    // console.log(ourTeam[i].picture);
+    // console.groupEnd();
+    
+}
+
+function generateCard(member){
+    const colEl = document.createElement('div');
+    colEl.className = 'col-4 d-flex justify-content-center';
+    // console.log(colEl)
+   colEl.innerHTML = `
+    <div class="card my-3">
+        <img src="img/${member.picture}" class="card-img-top">
+        <div class="card-body fst-italic">
+            <h5 class="card-title">${member.name}</h5>
+            <p class="card-text">${member.role}</p>
+        </div>
+    </div>`
+
+    return colEl;
+   
+    
     
 }
